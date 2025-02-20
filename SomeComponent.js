ff18715430c89ba05fb6ca0b85a154ce850d78f7
BugHunter-X <askzenai@gmail.com>
2025-02-20 15:46:04 +0100

    Add SomeComponent.js file

---
+++ SomeComponent.js
@@ -0,0 +1,7 @@
+```javascript
+// SomeComponent.js
+<div className="bg-gray-100 p-4 rounded-lg">
+  <h2 className="text-xl font-bold mb-2">Error: Incorrect class name</h2>
+  <p>This is where I expect my content to be, but Tailwind is not applying the styles.</p>
+</div>
+```
